5533760ed1f8f3e170ed16431b274d0c57aa4813
micmarolla <mic.marolla@gmail.com>
2023-11-26 10:03:21 +0100

    Lama flight mode is now integrated in mavlink

--- src/modules/commander/px4_custom_mode.h
+++ src/modules/commander/px4_custom_mode.h
@@ -172,6 +172,13 @@ static inline union px4_custom_mode get_px4_custom_mode(uint8_t nav_state)
 		custom_mode.main_mode = PX4_CUSTOM_MAIN_MODE_AUTO;
 		custom_mode.sub_mode = PX4_CUSTOM_SUB_MODE_AUTO_VTOL_TAKEOFF;
 		break;
+
+	// *** CUSTOM ***
+	case vehicle_status_s::NAVIGATION_STATE_LAMA:
+		custom_mode.main_mode = PX4_CUSTOM_MAIN_MODE_LAMA;
+		break;
+	
+	// *** END-CUSTOM ***
 	}
 
 	return custom_mode;
